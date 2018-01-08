ada6e923bff1764062d3bf4a8b5412f9515fd29f
github username <github email address>
2018-01-08 16:50:30 -0500

    Done.

--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,9 @@
-export default function manageUsers(state, action){
+export default function manageUsers(state = { users: [] }, action) {
+  switch (action.type) {
+    case "ADD_USER":
+      const users = [...state.users, action.user];
+      return { ...state, users: users };
+    default:
+      return state;
+  }
 }
