4de954991c72abf089136a96eef87f2d30fbaa59
Fabiano Cunha <cunhasb@aol.com>
2018-01-08 20:54:24 -0500

    Done.

--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,17 @@
-export default function manageUsers(state, action){
+export default function manageUsers(state, action) {
+  console.log("manageUsers", (state = { users: [] }), action);
+  switch (action.type) {
+    case "ADD_USER":
+      console.log(
+        "adduser=state",
+        state.users,
+        action.user.username,
+        action.user.hometown
+      );
+      return {
+        users: [...state.users, action.user]
+      };
+    default:
+      return state;
+  }
 }
